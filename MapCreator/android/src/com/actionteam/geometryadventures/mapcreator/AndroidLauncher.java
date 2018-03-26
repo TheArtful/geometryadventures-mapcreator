812e8acdd29bdb6eefc1f986106648a23464276e
Ahmed Essam <theartful.ae@gmail.com>
2018-03-26 16:02:54 +0200

    Create "Map Creator" project

---
+++ MapCreator/android/src/com/actionteam/geometryadventures/mapcreator/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.actionteam.geometryadventures.mapcreator;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.actionteam.geometryadventures.mapcreator.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
---
+++ MapCreator/desktop/src/com/actionteam/geometryadventures/mapcreator/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.actionteam.geometryadventures.mapcreator.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.actionteam.geometryadventures.mapcreator.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
