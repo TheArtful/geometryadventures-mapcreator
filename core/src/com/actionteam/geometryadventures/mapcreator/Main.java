909ba55d255db0308c1f56bc45b999623cb214dc
Ahmed Essam <theartful.ae@gmail.com>
2018-04-01 16:44:17 +0200

    Draw grid on screen

--- core/src/com/actionteam/geometryadventures/mapcreator/Main.java
+++ core/src/com/actionteam/geometryadventures/mapcreator/Main.java
@@ -1,5 +1,6 @@
 package com.actionteam.geometryadventures.mapcreator;
 
+import com.actionteam.geometryadventures.mapcreator.view.Grid;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -7,27 +8,27 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	Grid grid;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		grid = new Grid();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		grid.render(0);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height){
+		grid.setSize(width, height);
 	}
 }
