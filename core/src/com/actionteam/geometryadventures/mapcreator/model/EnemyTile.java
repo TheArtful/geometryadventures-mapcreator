dfa4abd32e80b14dcafb77fde3dd761ebf439b51
rka <enrev91@gmail.com>
2018-05-09 14:39:25 +0200

    minor change to map

--- core/src/com/actionteam/geometryadventures/mapcreator/model/EnemyTile.java
+++ core/src/com/actionteam/geometryadventures/mapcreator/model/EnemyTile.java
@@ -5,6 +5,6 @@ package com.actionteam.geometryadventures.mapcreator.model;
  */
 
 public class EnemyTile extends Tile {
-    public String subtype = "no weapon";
+    public String subtype = "green orc";
     public int health;
 }
